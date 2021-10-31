3c340c59512277a729410441436412f23f352780
unknown <son24051994@gmail.com>
2021-10-31 14:07:57 +0700

    Lesson02

--- Lesson02/hw_use_workbench.sql
+++ Lesson02/hw_use_workbench.sql
@@ -2,7 +2,7 @@
 CREATE SCHEMA `student_cms_plusplus` ;
 
 -- tao table student
-CREATE TABLE `studen_cms_plusplus`.`student` (
+CREATE TABLE `student_cms_plusplus`.`student` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(45) NOT NULL,
   `mssv` INT NOT NULL,
@@ -16,7 +16,7 @@ CREATE TABLE `studen_cms_plusplus`.`student` (
   PRIMARY KEY (`id`));
 
 -- tao table class
-CREATE TABLE `studen_cms_plusplus`.`class` (
+CREATE TABLE `student_cms_plusplus`.`class` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(45) NOT NULL,
   `major` VARCHAR(45) NOT NULL,
@@ -28,7 +28,7 @@ CREATE TABLE `studen_cms_plusplus`.`class` (
   PRIMARY KEY (`id`));
   
   -- tao table student_class
-  CREATE TABLE `studen_cms_plusplus`.`student_class` (
+  CREATE TABLE `student_cms_plusplus`.`student_class` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `student_id` INT NOT NULL,
   `class_id` INT NOT NULL,
