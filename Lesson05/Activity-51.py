245fb7437aecd72b5d8fd89e31aa514ac0604e0b
unknown <son24051994@gmail.com>
2021-11-13 13:55:48 +0700

    update

--- Lesson05/Activity-51.py
+++ Lesson05/Activity-51.py
@@ -40,7 +40,25 @@ def seach_laptop(name):
   mycursor.execute(sql)
 
   myresult = mycursor.fetchall()
+  my_list = []
   for x in myresult:
-    print(x)
+    my_list.append(x)
+  
+  for i in my_list:
+    id = i[0]
+    name = i[1]
+    maker = i[3]
+    type = i[4]
+    ram = i[5]
+    cpu = i[6]
+    ssd = i[7]
+    hdd = i[8]
+    price = i[9]
+    card = i[10]
+    screen_resolution = i[11]
+    screen_size = i[12]
+    sold = i[13]
+    print("id: %s, name: %s, maker: %s, type: %s, ram: %s, cpu: %s, ssd: %s, hdd: %s, price: %s, card: %s, screen_resolution: %s, screen_size: %s, sold: %s" %
+        (id, name, maker, type, ram, cpu, ssd, hdd, price, card, screen_resolution, screen_size, sold))
   
 seach_laptop('Laptop ASUS ROG Zephyrus S GX531GM-ES004T (15.6" FHD/i7-8750H/16GB/512GB SSD/GTX 1060/Win10/2.1 kg)')
